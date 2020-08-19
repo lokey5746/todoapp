78b480e8e6d0864b068eb8bd166cfdd1846247be
Lokendra Dangi <pachar.com@gmail.com>
2020-08-19 22:32:17 +0545

    Fetch Data from array with map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState(["Cook Food", "Take Dog out !!"]);
+  const [input, setInput] = useState("");
   return (
     <div className="App">
       <h2>Todo-App with React and Firebase</h2>
+      <input />
+      <button>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
